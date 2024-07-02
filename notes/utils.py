0ca324262a5d603233832ec49e3c845219d2b9d6
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-07-03 00:01:47 +0200

    OheHotEncoder

--- notes/utils.py
+++ notes/utils.py
@@ -1,6 +1,11 @@
+from copy import deepcopy
+
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 
+
+from functools import reduce
 
 def score_model(x_train, x_test, y_train, y_val, model=DecisionTreeRegressor):
     model = model(random_state=0)
@@ -8,3 +13,19 @@ def score_model(x_train, x_test, y_train, y_val, model=DecisionTreeRegressor):
 
     preds = model.predict(x_test)
     return mean_absolute_error(y_val, preds)
+
+
+def split_test_dataset(data, types='numerical'):
+    df_copy = {}
+    if types == 'all':
+        df_copy = deepcopy(data)
+    elif types == 'numerical':
+        df_copy = deepcopy(data[data.describe().columns])
+    elif types == 'categorical':
+        df_copy = deepcopy(data[data.describe().columns])
+
+    X_train = df_copy.drop(columns=['Price'])
+    y_train = df_copy.Price
+
+    return train_test_split(X_train, y_train, random_state=1)
+
